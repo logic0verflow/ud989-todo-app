f6bf39d2cc2063010feff435b7bf4ab6ded23dc4
logic0verflow <git@nathanacosta.com>
2017-08-25 11:07:02 -0600

    Update with " -- done " and add edit button

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -19,6 +19,7 @@ var app = app || {};
 		events: {
 			'click .toggle': 'toggleCompleted',
 			'dblclick label': 'edit',
+			'click .edit-btn': 'edit',
 			'click .destroy': 'clear',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
@@ -32,6 +33,7 @@ var app = app || {};
 		initialize: function () {
 			this.listenTo(this.model, 'change', this.render);
 			this.listenTo(this.model, 'destroy', this.remove);
+
 			this.listenTo(this.model, 'visible', this.toggleVisible);
 		},
 
